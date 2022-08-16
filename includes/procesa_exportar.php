665ad3b9f05bc648023b0a5269f0cc653fe1ffb4
Ivan Ruiz Delfin <ivanruizdelfin@gmail.com>
2022-08-16 11:22:54 -0400

    exportar a excel

---
+++ includes/procesa_exportar.php
@@ -0,0 +1,43 @@
+<?php
+
+    require "../vendor/autoload.php";
+    include_once "../conexion/conexion.php";
+
+    use PhpOffice\PhpSpreadsheet\{Spreadsheet, IOFactory};
+
+    $sentencia = $base_de_datos->query("select id, nombre, edad, fecha from mascotas");
+    $mascotas = $sentencia->fetchAll(PDO::FETCH_OBJ);
+
+    $excel = new Spreadsheet();
+    $hojaActiva = $excel->getActiveSheet();
+    $hojaActiva->setTitle("Mascotas");
+
+    $hojaActiva->getColumnDimension('A')->setWidth(20);
+    $hojaActiva->setCellValue('A1', 'ID');
+    $hojaActiva->getColumnDimension('B')->setWidth(20);
+    $hojaActiva->setCellValue('B1', 'NOMBRE');
+    $hojaActiva->getColumnDimension('C')->setWidth(20);
+    $hojaActiva->setCellValue('C1', 'EDAD');
+    $hojaActiva->getColumnDimension('D')->setWidth(20);
+    $hojaActiva->setCellValue('D1', 'FECHA DE NACIMIENTO');
+
+    $fila = 2;
+
+    foreach($mascotas as $mascota){
+        
+        $hojaActiva->setCellValue('A'.$fila, $mascota->id);
+        $hojaActiva->setCellValue('B'.$fila, $mascota->nombre);
+        $hojaActiva->setCellValue('C'.$fila, $mascota->edad);
+        $hojaActiva->setCellValue('D'.$fila, $mascota->fecha);
+
+        $fila++;
+
+    } 
+
+    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    header('Content-Disposition: attachment;filename="mascotas.xlsx"');
+    header('Cache-Control: max-age=0');
+    
+    $writer = IOFactory::createWriter($excel, 'Xlsx');
+    $writer->save('php://output');
+?>
